09f133ae849c26a569f87bb2ef909c111d86a537
otavio augusto <tavigamer08@gmail.com>
2026-03-04 22:13:53 -0300

    Implement parking fee calculation for multiple vehicles
    Added functionality to calculate parking fees for two vehicles.

--- Main.java
+++ Main.java
@@ -1,14 +1,41 @@
 public class Main {
-
+   static Integer valorHora = 5;
     public static void main(String[] args) {
 
+        Veiculo c1 = new Veiculo();
+        c1.cor = "azul";
+        c1.placa = "REBORN";
+        c1.horaChegada = 12;
+        c1.horaSaida = 32;
+        Integer qtdHoras = c1.calcularHorasEst();
+        Integer valorPago = qtdHoras*valorHora;
         
-        Carro carro1 = new Carro("Toyota", "Corolla", 2022, 100000);
-        Carro carro2 = new Carro("Honda", "Civic", 2023, 120000);
-
+        System.out.println("Dados do carro1: ");
+        System.out.println("Cor do carro: " + c1.cor);
+        System.out.println("Placa do carro: " + c1.placa);
+        System.out.println("Hora da chegada: " + c1.horaChegada);
+        System.out.println("Hora da Saida:" + c1.horaSaida);
+        System.out.println("Horas Estacionadas: " + qtdHoras);
+        System.out.println("Valor a pagar: R$ " + valorPago);
         
-        carro1.exibirDados();
         System.out.println();
-        carro2.exibirDados();
+        
+        Veiculo c2 = new Veiculo();
+        c2.cor = "Branco";
+        c2.placa = "Piloto";
+        c2.horaChegada = 04;
+        c2.horaSaida = 18;
+        Integer qtdHoras2 = c2.calcularHorasEst();
+        Integer valorPago2 = qtdHoras2*valorHora;
+        
+        System.out.println("Dados do carro2: ");
+        System.out.println("Cor do carro: " + c2.cor);
+        System.out.println("Placa do carro: " + c2.placa);
+        System.out.println("Hora da chegada: " + c2.horaChegada);
+        System.out.println("Hora da Saida:" + c2.horaSaida);
+        System.out.println("Horas Estacionadas: " + qtdHoras2);
+        System.out.println("Valor a pagar: R$ " + valorPago2);
+        
+        
     }
 }
